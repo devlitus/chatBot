be801e8477e2b37f0398897ff02ccac2521c29d0
devlitus <developercarles@gmail.com>
2025-03-23 16:57:58 +0100

    add env gitignore

--- src/constants.ts
+++ src/constants.ts
@@ -2,3 +2,6 @@ export const GROQ_URL_MODELS: string = 'https://api.groq.com/openai/v1/models';
 export const GROQ_URL_COMPLETION: string = 'https://api.groq.com/openai/v1/chat/completions';
 export const GROQ_API_KEY: string = import.meta.env.VITE_GROQ_API_KEY;
 
+export const SUPABASE_URL: string = import.meta.env.VITE_SUPABASE_URL;
+export const SUPABASE_ANON_KEY: string = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,6 +2,8 @@
 //tipar las variables de entorno
 interface ImportMetaEnv {
   readonly VITE_GROQ_API_KEY: string;
+  readonly VITE_SUPABASE_URL: string;
+  readonly VITE_SUPABASE_ANON_KEY: string;
 }
 
 interface ImportMeta {
