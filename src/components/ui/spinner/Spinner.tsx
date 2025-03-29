90f11dc9121eb245e9823a90f9b2ef59d0fed0a7
devlitus <developercarles@gmail.com>
2025-03-29 11:34:29 +0100

    test: añade data-testid al componente Spinner para mejorar testabilidad

--- src/components/ui/spinner/Spinner.tsx
+++ src/components/ui/spinner/Spinner.tsx
@@ -2,8 +2,8 @@ import './Spinner.css';
 
 const Spinner = () => {
   return (
-    <div className="spinner-container">
-      <div className="spinner"></div>
+    <div className="spinner-container" data-testid="spinner-container">
+      <div className="spinner" data-testid="spinner"></div>
     </div>
   );
 };
