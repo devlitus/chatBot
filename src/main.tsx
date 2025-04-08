22e67dc153eb7c04411c7c2da9f517a492b642f9
Carles Pedrero <developercarles@gmail.com>
2025-04-08 20:46:14 +0200

    Inicializar Sentry en el punto de entrada

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,14 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import * as Sentry from '@sentry/react';
-import './global.css';
-import { App } from './App.tsx';
-import { SENTRY_DSN } from './constants.ts';
-Sentry.init({
-  dsn: SENTRY_DSN,
-  environment: import.meta.env.MODE,
-  integrations: [Sentry.browserTracingIntegration()],
-  tracesSampleRate: 1.0,
-  tracePropagationTargets: [
-    'localhost',
-    'https://chat-bot-wine-five.vercel.app/',
-  ],
-});
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { initSentry } from './utils/sentry';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+// Inicializar Sentry
+initSentry();
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 );
