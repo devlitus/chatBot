301bc12a6e548c8d050ccc1f8173459d1f3c7a0a
devlitus <developercarles@gmail.com>
2025-04-21 10:15:03 +0200

    fix: actualizar variables de entorno en vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,10 +2,7 @@
 //tipar las variables de entorno
 interface ImportMetaEnv {
   readonly VITE_GROQ_API_KEY: string;
-  
-  readonly VITE_SUPABASE_URL: string;
-  readonly VITE_SUPABASE_ANON_KEY: string;
-
+  readonly VITE_SUPABASE_ACCESS_TOKEN: string;
   readonly VITE_SENTRY_DSN: string;
   readonly VITE_SENTRY_AUTH_TOKEN: string;
   readonly VITE_SENTRY_ENVIRONMENT: string;
