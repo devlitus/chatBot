a3b1898f9453f13e7358a597c9ce179c0de81ecc
devlitus <developercarles@gmail.com>
2025-04-21 10:14:57 +0200

    fix: corregir constantes de Supabase en constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -2,8 +2,7 @@ export const GROQ_URL_MODELS: string = 'https://api.groq.com/openai/v1/models';
 export const GROQ_URL_COMPLETION: string = 'https://api.groq.com/openai/v1/chat/completions';
 export const GROQ_API_KEY: string = import.meta.env.VITE_GROQ_API_KEY;
 
-export const SUPABASE_URL: string = import.meta.env.VITE_SUPABASE_URL;
-export const SUPABASE_ANON_KEY: string = import.meta.env.VITE_SUPABASE_ANON_KEY;
+export const SUPABASE_ACCESS_TOKEN = import.meta.env.VITE_SUPABASE_ACCESS_TOKEN;
 
 export const SENTRY_DSN: string = import.meta.env.VITE_SENTRY_DSN;
 export const SENTRY_AUTH_TOKEN: string = import.meta.env.VITE_SENTRY_AUTH_TOKEN;
