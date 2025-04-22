839f8ce400621f98ccf97c5078d900738e96abfc
devlitus <developercarles@gmail.com>
2025-04-22 10:17:52 +0200

    fix: actualizar la tipificación de variables de entorno para incluir las constantes de Sentry

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,10 +2,15 @@
 //tipar las variables de entorno
 interface ImportMetaEnv {
   readonly VITE_GROQ_API_KEY: string;
-  readonly VITE_SUPABASE_ACCESS_TOKEN: string;
+
+  readonly VITE_SUPABASE_URL: string;
+  readonly VITE_SUPABASE_ANON_KEY: string;
+
   readonly VITE_SENTRY_DSN: string;
   readonly VITE_SENTRY_AUTH_TOKEN: string;
   readonly VITE_SENTRY_ENVIRONMENT: string;
+  readonly VITE_SENTRY_ORG: string;
+  readonly VITE_SENTRY_PROJECT: string;
 }
 
 interface ImportMeta {
