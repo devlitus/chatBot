f87566127c2a7138027d8363507693e1a53fcde0
devlitus <developercarles@gmail.com>
2025-06-21 10:03:45 +0200

    feat: Refactor GEMINI_API_KEY import and add environment mock for testing

--- src/pages/api/chat.ts
+++ src/pages/api/chat.ts
@@ -1,5 +1,5 @@
 import { GoogleGenAI } from "@google/genai";
-import {GEMINI_API_KEY} from "astro:env/server";
+import { GEMINI_API_KEY } from "../../utils/env";
  
 
 export const prerender = false;
